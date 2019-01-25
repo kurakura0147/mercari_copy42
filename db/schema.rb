5efde0824a32c224eb18e4fb8eaeb5d29097bb1e
t-naganuma <takuya.naganuma@gmail.com>
2019-01-25 15:25:38 +0900

    Add Address Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190124082716) do
+ActiveRecord::Schema.define(version: 20190124095017) do
+
+  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "postal_code",   null: false
+    t.string   "prefecture",    null: false
+    t.string   "city",          null: false
+    t.string   "house_number",  null: false
+    t.string   "building_name"
+    t.integer  "user_id",       null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                            null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 20190124082716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "addresses", "users"
 end
