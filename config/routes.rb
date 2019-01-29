9e1adc2f2284c102981c7d952b32e8375b0ddc43
kurayoshi981 <tachp981@yahoo.co.jp>
2019-01-29 18:51:10 +0900

    feat:商品購入確認ページのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  resources :items, only: [:index, :new]
   resources :users, only: [:new , :show] do
     member do
       get 'signout'
