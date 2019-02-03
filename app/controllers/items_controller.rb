59981fd666a44b1b3cda4738bc9ea944c4f8a6fd
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-02-03 15:06:36 +0900

    Edit instans変数

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-          @items1 = Item.where(price: 1).limit(4).order("id DESC")
-          @items2 = Item.where(price: 2).limit(4).order("id DESC")
-          @items3 = Item.where(price: 3).limit(4).order("id DESC")
-          @items4 = Item.where(price: 4).limit(4).order("id DESC")
+          @price1_items = Item.where(price: 1).limit(4).order("id DESC")
+          @price2_items = Item.where(price: 2).limit(4).order("id DESC")
+          @price3_items = Item.where(price: 3).limit(4).order("id DESC")
+          @price4_items = Item.where(price: 4).limit(4).order("id DESC")
   end
 
   def new
