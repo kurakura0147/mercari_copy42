c998ec11ddcf112db50142c6165ddf8d30fc1017
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-02-03 14:20:10 +0900

    Edit_for method（item_index）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,20 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
+          @items1 = Item.where(price: 1).limit(4).order("id DESC")
+          @items2 = Item.where(price: 2).limit(4).order("id DESC")
+          @items3 = Item.where(price: 3).limit(4).order("id DESC")
+          @items4 = Item.where(price: 4).limit(4).order("id DESC")
   end
 
   def new
+
   end
 
   def show
+
   end
 
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :item do
+    name "abe"
+    image "@@@@@.png"
+    detail "test"
+    state "test"
+    price "1"
+    delivery_cost "00"
+    delivery_area "00"
+    delivery_day "00"
+    size "000"
+    user_id "000"
+  end
+end
