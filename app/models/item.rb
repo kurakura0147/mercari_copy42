c871fa87c40a9130a1baa61044ed5f42e8c652ae
t-naganuma <takuya.naganuma@gmail.com>
2019-02-05 00:56:08 +0900

    Test: Add Destroy Action Test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  #商品出品機能が完成後にコメントアウトを外す
+  # mount_uploader :image, ImageUploader
   belongs_to :user
-  belongs_to :brand
-  belongs_to :category
+  # belongs_to :brand
+  # belongs_to :category
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,6 +6,11 @@ FactoryGirl.define do
     email                  Faker::Internet.free_email
     password               password
     password_confirmation  password
+    last_name              Faker::Pokemon.name
+    first_name             Faker::Pokemon.name
+    last_name_kana         Faker::Pokemon.name
+    first_name_kana        Faker::Pokemon.name
+    phone_number           000011112222
   end
 
 end
