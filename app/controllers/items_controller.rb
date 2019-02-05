8a8994d684a2b644cec60ab6d66dc4aa9603d068
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-02-06 02:28:52 +0900

    modelにmethodの切り分け実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @price1_items = Item.where(price: 1).limit(4).order("id DESC")
-    @price2_items = Item.where(price: 2).limit(4).order("id DESC")
-    @price3_items = Item.where(price: 3).limit(4).order("id DESC")
-    @price4_items = Item.where(price: 4).limit(4).order("id DESC")
+    @price1_items = Item.set_items(1)
+    @price2_items = Item.set_items(2)
+    @price3_items = Item.set_items(3)
+    @price4_items = Item.set_items(4)
   end
 
   def new
