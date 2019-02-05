1eb88c93874e05cf1ececf9f3da9d9d108daa2c3
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-02-05 18:31:31 +0900

    Add action of item_show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-          @price1_items = Item.where(price: 1).limit(4).order("id DESC")
-          @price2_items = Item.where(price: 2).limit(4).order("id DESC")
-          @price3_items = Item.where(price: 3).limit(4).order("id DESC")
-          @price4_items = Item.where(price: 4).limit(4).order("id DESC")
+    @price1_items = Item.where(price: 1).limit(4).order("id DESC")
+    @price2_items = Item.where(price: 2).limit(4).order("id DESC")
+    @price3_items = Item.where(price: 3).limit(4).order("id DESC")
+    @price4_items = Item.where(price: 4).limit(4).order("id DESC")
   end
 
   def new
@@ -13,7 +13,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-
+    @item = Item.find(params[:id])
+    @user_items = Item.where(user_id: @item.user_id).sample(6)
+    #brand_idをテーブルに追加後コメントアウトを外す
+    # @brand_items = Item.where(brand_id: @item.brans_id).sample(6)
   end
 
 end
